0e6476c1d8efd05f9bcac1a6629e6fe2d228e7ee
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 04:25:06 +0200

    useFetch as array

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useFetch = (url) => {
+export const useFetch = (url, mockWait = false) => {
 	const [items, setItems] = useState([]);
 
 	useEffect(() => {
 		(async () => {
-			setItems((await axios.get(url)).data);
+			const _items = (await axios.get(url)).data;
+			if (mockWait) {
+				setTimeout(() => {
+					setItems(_items);
+				}, 3000);
+			} else {
+				setItems(_items);
+			}
 		})();
 	}, []);
 
-	return [items];
+	return [items, items.length, items.length === 0];
 };
--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,19 +1,42 @@
 import { useFetch } from '../hooks/useFetch';
 
 export const PageBooks = () => {
-	const [techBooks] = useFetch('https://edwardtanguay.netlify.app/share/techBooks.json');
+	const [techBooks, totalTechBooks, techBooksAreLoading] = useFetch(
+		'https://edwardtanguay.netlify.app/share/techBooks.json',
+		true
+	);
+	const [generalBooks, whatever, generalBooksAreLoading] = useFetch(
+		'https://edwardtanguay.netlify.app/share/books.json', true
+	);
 
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
-			<h3>There are {techBooks.length} tech books.</h3>
-			<ul>
-				{techBooks.map((techBook, index) => {
-					return (
-						<li key={index}>{techBook.title}</li>
-					)
-				})}
-			</ul>
+			{techBooksAreLoading ? (
+				<h3>Tech books are loading...</h3>
+			) : (
+				<>
+					<h3>There are {totalTechBooks} tech books.</h3>
+					<ul>
+						{techBooks.map((techBook, index) => {
+							return <li key={index}>{techBook.title}</li>;
+						})}
+					</ul>
+				</>
+			)}
+
+			{generalBooksAreLoading ? (
+				<h3>General books are loading...</h3>
+			) : (
+				<>
+					<h3>There are {generalBooks.length} general books.</h3>
+					<ul>
+						{generalBooks.map((generalBook, index) => {
+							return <li key={index}>{generalBook.title}</li>;
+						})}
+					</ul>
+				</>
+			)}
 		</div>
-	)
-}
+	);
+};
