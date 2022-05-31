252d8aa17320314d299c9570e0d254b1a5ddf5ef
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 03:58:06 +0200

    useFetch works

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,19 @@
+import { useFetch } from '../hooks/useFetch';
+
 export const PageBooks = () => {
+	const [techBooks] = useFetch('https://edwardtanguay.netlify.app/share/techBooks.json');
+
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
-			<p>This is the book page.</p>
+			<h3>There are {techBooks.length} tech books.</h3>
+			<ul>
+				{techBooks.map((techBook, index) => {
+					return (
+						<li key={index}>{techBook.title}</li>
+					)
+				})}
+			</ul>
 		</div>
 	)
 }
