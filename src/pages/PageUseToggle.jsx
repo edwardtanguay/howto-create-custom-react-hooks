4846355ddbf9707cbc5cadb7c3292e5189c3a7a4
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-31 03:38:46 +0200

    useToggle finished

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -2,6 +2,8 @@ import { useToggle } from '../hooks/useToggle';
 
 export const PageUseToggle = () => {
 	const [userIsOnline, toggleUserIsOnline] = useToggle(false);
+	const [layoutIsDark, toggleLayoutIsDark] = useToggle(true);
+	const [projectIsFinished, toggleProjectIsFinished] = useToggle(false);
 
 	return (
 		<div className="page_useToggle">
@@ -13,9 +15,23 @@ export const PageUseToggle = () => {
 				<p>User is logged out.</p>
 			)}
 
-			<button onClick={() => toggleUserIsOnline()}>Toggle online status</button>
+			{layoutIsDark ? <p>Layout is dark.</p> : <p>Layout is light.</p>}
+
+			{projectIsFinished ? <p>Project has been completed.</p> : <p>Project is not finished.</p>}
+
+			<button onClick={() => toggleUserIsOnline()}>
+				Toggle online status
+			</button>
 			<button onClick={() => toggleUserIsOnline(true)}>logon</button>
 			<button onClick={() => toggleUserIsOnline(false)}>logoff</button>
+			<hr/>
+			<button onClick={() => toggleLayoutIsDark()}>Toggle layout</button>
+			<button onClick={() => toggleLayoutIsDark(true)}>Set layout to dark</button>
+			<button onClick={() => toggleLayoutIsDark(false)}>Set layout to light</button>
+			<hr/>
+			<button onClick={() => toggleProjectIsFinished()}>Toggle project status</button>
+			<button onClick={() => toggleProjectIsFinished(true)}>Set project to finished</button>
+			<button onClick={() => toggleProjectIsFinished(false)}>Set project to unfinished</button>
 		</div>
 	);
 };
